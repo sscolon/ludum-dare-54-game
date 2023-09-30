5caaad0b470385aec4190a16b3d3000265682d9c
Scolon <49799704+sscolon@users.noreply.github.com>
2023-09-30 14:55:07 -0400

    PauseManager

---
+++ Assets/Scripts/Project/Content/UI/PauseUI.cs
@@ -0,0 +1,38 @@
+﻿using ProjectBubble.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ProjectBubble.Content.UI
+{
+    public class PauseUI : MonoBehaviour
+    {
+        [SerializeField] private RectTransform _rootParent;
+        private void Start()
+        {
+            PauseManager.OnPause += ShowUI;
+            PauseManager.OnUnPause += HideUI;
+        }
+
+        private void OnDestroy()
+        {
+            PauseManager.OnPause -= ShowUI;
+            PauseManager.OnUnPause -= HideUI;
+        }
+
+        private void ShowUI()
+        {
+            //VFX later.
+            _rootParent.gameObject.SetActive(true);
+        }
+
+        private void HideUI()
+        {
+            //VFX later
+            _rootParent.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Project/Core/PauseManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace ProjectBubble.Core
+{
+    public static class PauseManager
+    {
+        public static event Action OnPause;
+        public static event Action OnUnPause;
+        public static void Pause()
+        {
+            Time.timeScale = 0;
+            OnPause?.Invoke();
+        }
+
+        public static void UnPause()
+        {
+            Time.timeScale = 1;
+            OnUnPause?.Invoke();
+        }
+    }
+}
